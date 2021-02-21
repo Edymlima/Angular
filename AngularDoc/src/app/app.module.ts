c4f56af6796e15b951e5d6a629d401b150179f70
Edilberto Lima <edilberto.lima@sicoobprevi.com.br>
2021-02-21 15:36:52 -0300

    Ajuste de Estrutura

--- AngularDoc/src/app/app.module.ts
+++ AngularDoc/src/app/app.module.ts
@@ -3,12 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PropertyBindingComponent } from './property-binding/property-binding.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent,
-    PropertyBindingComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
